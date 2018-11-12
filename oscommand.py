e2b548fdbbae894f32f406e9c366a4d528c83d09
wanghj <78732833@qq.com>
2018-11-12 11:02:15 +0800

    test script

---
+++ oscommand.py
@@ -0,0 +1,10 @@
+import os
+
+
+print(os.system('git add .'))
+
+print(os.system('git pull origin  master'))
+
+print(os.system('git commit -m "test script"'))
+
+print(os.system('git push -u origin master'))
